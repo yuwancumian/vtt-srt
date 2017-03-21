6c24bd8df4e1d9f80ba2ee97d22b613f376be156
yuwancumian <272157218@qq.com>
2017-03-21 11:15:14 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+var os = require('os')
+var fs = require('mz/fs')
+var exec = require('mz/child_process').exec
+
+
+// fs.readdir('./').then(function(data){
+//   var len = data.length
+//   var arr = []
+//   for (var i = 0; i< len; i++){
+//     var filename = data[i].split('.').pop()
+//     if (filename === 'vtt'){
+//       arr.push(data[i]) 
+//     }
+//   }
+//   return arr
+// }).then(function(data){
+//   exec('sed -i ".bak" "1d" *.vtt ')
+//   exec('rm *.bak')
+// })
+
+exec('sed -i ".bak" "1d" *.vtt ')
+exec('rm *.bak')
+fs.readdir('./').then(function(data){
+  var len = data.length
+  var arr = []
+  for (var i = 0; i< len; i++){
+    var filename = data[i].split('.').pop()
+    if (filename === 'vtt'){
+      arr.push(data[i]) 
+    }
+  }
+  return(arr)
+}).then(function(vttArray){
+  console.log(vttArray)
+  var len = vttArray.length
+  for (let j = 0; j< len; j++){
+    (function(){
+      var filename = vttArray[j].split('.')[0] + '.srt'
+      console.log('filename:' + filename)
+      fs.readFile(vttArray[j]).then(function(data){
+        return data
+      }).then(function(fileContent){
+
+        console.log('filecontent' +fileContent)
+        fs.writeFile(filename, fileContent).then(function(){
+
+        }) 
+      }).then(function(){
+        if (j == len-1){
+          exec('rm *.vtt')
+          console.log('Done!')
+        }
+      })
+    })()
+  }
+})
